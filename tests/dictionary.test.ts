9e45fd7627ce8846f589baf4493d871c8622ee19
steven nguyen <icecream17.github@gmail.com>
2021-02-22 10:31:22 -0600

    Update dictionary.test.ts

--- tests/dictionary.test.ts
+++ tests/dictionary.test.ts
@@ -63,4 +63,227 @@ describe('Specification', () => {
       expect(Specification.AbstractOperations.SameValue(null, null)).toBe(true)
       expect(Specification.AbstractOperations.SameValue(NaN, NaN)).toBe(true)
    })
+
+   test('IsAccessorDescriptor', () => {
+      expect(Specification.AbstractOperations.IsAccessorDescriptor({
+         get() {return 7},
+         set() {}
+      })).toBe(true)
+      expect(Specification.AbstractOperations.IsAccessorDescriptor({
+         get() {return 7}
+      })).toBe(true)
+      expect(Specification.AbstractOperations.IsAccessorDescriptor({
+         set() {}
+      })).toBe(true)
+      expect(Specification.AbstractOperations.IsAccessorDescriptor({})).toBe(false)
+      expect(Specification.AbstractOperations.IsAccessorDescriptor({enumerable: true})).toBe(false)
+   })
+
+   test('IsDataDescriptor', () => {
+      expect(Specification.AbstractOperations.IsDataDescriptor({
+         value: 7,
+         writable: true
+      })).toBe(true)
+      expect(Specification.AbstractOperations.IsDataDescriptor({
+         value: false,
+         writable: false
+      })).toBe(true)
+      expect(Specification.AbstractOperations.IsDataDescriptor({
+         value: false
+      })).toBe(true)
+      expect(Specification.AbstractOperations.IsDataDescriptor({
+         writable: false
+      })).toBe(true)
+      expect(Specification.AbstractOperations.IsDataDescriptor({})).toBe(false)
+      expect(Specification.AbstractOperations.IsDataDescriptor({ enumerable: true })).toBe(false)
+   })
+   
+   test('IsGenericDescriptor', () => {
+      expect(Specification.AbstractOperations.IsGenericDescriptor({
+         get() {return 7},
+         set() {}
+      })).toBe(false)
+      expect(Specification.AbstractOperations.IsGenericDescriptor({
+         get() {return 7}
+      })).toBe(false)
+      expect(Specification.AbstractOperations.IsGenericDescriptor({
+         set() {}
+      })).toBe(false)
+      expect(Specification.AbstractOperations.IsGenericDescriptor({
+         value: 7,
+         writable: true
+      })).toBe(false)
+      expect(Specification.AbstractOperations.IsGenericDescriptor({
+         value: false,
+         writable: false
+      })).toBe(false)
+      expect(Specification.AbstractOperations.IsGenericDescriptor({
+         value: false
+      })).toBe(false)
+      expect(Specification.AbstractOperations.IsGenericDescriptor({
+         writable: false
+      })).toBe(false)
+
+      expect(Specification.AbstractOperations.IsGenericDescriptor({
+         enumerable: true
+      })).toBe(true)
+      expect(Specification.AbstractOperations.IsGenericDescriptor({
+         enumerable: false
+      })).toBe(true)
+      expect(Specification.AbstractOperations.IsGenericDescriptor({
+         configurable: false
+      })).toBe(true)
+      expect(Specification.AbstractOperations.IsGenericDescriptor({
+         configurable: false,
+         enumerable: false
+      })).toBe(true)
+      expect(Specification.AbstractOperations.IsGenericDescriptor({})).toBe(true)
+   })
+
+   test('IsCompatiblePropertyDescriptor', () => {
+      expect(Specification.AbstractOperations.IsCompatiblePropertyDescriptor(true, {}, undefined)).toBe(true)
+      expect(Specification.AbstractOperations.IsCompatiblePropertyDescriptor(false, {}, undefined)).toBe(false)
+      expect(Specification.AbstractOperations.IsCompatiblePropertyDescriptor(false, {}, {})).toBe(true)
+      expect(Specification.AbstractOperations.IsCompatiblePropertyDescriptor(
+         true,
+         {
+            configurable: true
+         },
+         {
+            configurable: false
+         }
+      )).toBe(false)
+      expect(Specification.AbstractOperations.IsCompatiblePropertyDescriptor(
+         true,
+         {
+            enumerable: false
+         },
+         {
+            configurable: false,
+            enumerable: true
+         }
+      )).toBe(false)
+      expect(Specification.AbstractOperations.IsCompatiblePropertyDescriptor(
+         true,
+         {
+            enumerable: true
+         },
+         {
+            configurable: false,
+            enumerable: false
+         }
+      )).toBe(false)
+      expect(Specification.AbstractOperations.IsCompatiblePropertyDescriptor(
+         false,
+         {
+            enumerable: true
+         },
+         {
+            configurable: false,
+            enumerable: true
+         }
+      )).toBe(true)
+      expect(Specification.AbstractOperations.IsCompatiblePropertyDescriptor(
+         false,
+         {
+            enumerable: true
+         },
+         {
+            configurable: false,
+            enumerable: true,
+            value: 7
+         }
+      )).toBe(false)
+      expect(Specification.AbstractOperations.IsCompatiblePropertyDescriptor(
+         false,
+         {
+            enumerable: true,
+            value: 7
+         },
+         {
+            configurable: false,
+            enumerable: true
+         }
+      )).toBe(false)
+      expect(Specification.AbstractOperations.IsCompatiblePropertyDescriptor(
+         false,
+         {
+            enumerable: true,
+            value: 7
+         },
+         {
+            configurable: false,
+            enumerable: true,
+            writable: false,
+            value: 7
+         }
+      )).toBe(true)
+      expect(Specification.AbstractOperations.IsCompatiblePropertyDescriptor(
+         false,
+         {
+            enumerable: true,
+            value: 'Not equal'
+         },
+         {
+            configurable: false,
+            enumerable: true,
+            writable: false,
+            value: 'Different'
+         }
+      )).toBe(false)
+      expect(Specification.AbstractOperations.IsCompatiblePropertyDescriptor(
+         false,
+         {
+            enumerable: true,
+            value: 0
+         },
+         {
+            configurable: false,
+            enumerable: true,
+            writable: false,
+            value: -0
+         }
+      )).toBe(false)
+      expect(Specification.AbstractOperations.IsCompatiblePropertyDescriptor(
+         false,
+         {
+            enumerable: true,
+            writable: true,
+            value: 0
+         },
+         {
+            configurable: false,
+            enumerable: true,
+            writable: false,
+            value: 0
+         }
+      )).toBe(false)
+      expect(Specification.AbstractOperations.IsCompatiblePropertyDescriptor(
+         false,
+         {
+            enumerable: true,
+            get: Number
+         },
+         {
+            configurable: false,
+            enumerable: true,
+            get: Number,
+            set: Number
+         }
+      )).toBe(false)
+      expect(Specification.AbstractOperations.IsCompatiblePropertyDescriptor(
+         false,
+         {
+            enumerable: true,
+            get: Number,
+            set: Number
+         },
+         {
+            configurable: false,
+            enumerable: true,
+            get: Number,
+            set: Number
+         }
+      )).toBe(true)
+   })
 })
