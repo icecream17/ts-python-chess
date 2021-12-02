63c1b630de4f1687771003c2beb813a0dae1b7b9
steven nguyen <nguyeste008@students.garlandisd.net>
2021-12-01 19:02:25 -0600

    Update success.js

--- success.js
+++ success.js
@@ -19,7 +19,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-import chalk from 'chalk'
+const chalk = require('chalk')
 
 // const args = process.argv.slice(2)
 
