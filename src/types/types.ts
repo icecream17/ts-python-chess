9e6c11c9c007cfb2d1abeda672cca811937f0282
Steven Nguyen <nguyeste008@students.garlandisd.net>
2022-04-25 12:34:31 -0500

    _EnPassantSpec + double quote [skip ci]

--- src/types/types.ts
+++ src/types/types.ts
@@ -18,11 +18,13 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+export type _EnPassantSpec = "legal" | "fen" | "xfen"
+
 /** The side to move or color of a piece */
 export type Color = boolean
 export type PieceType = 1 | 2 | 3 | 4 | 5 | 6
-export type PieceLetter = 'p' | 'n' | 'b' | 'r' | 'q' | 'k'
-export type PieceName = 'pawn' | 'knight' | 'bishop' | 'rook' | 'queen' | 'king'
+export type PieceLetter = "p" | "n" | "b" | "r" | "q" | "k"
+export type PieceName = "pawn" | "knight" | "bishop" | "rook" | "queen" | "king"
 export type Square =
     0 |  1 |  2 |  3 |  4 |  5 |  6 |  7 |
     8 |  9 | 10 | 11 | 12 | 13 | 14 | 15 |
@@ -34,14 +36,14 @@ export type Square =
    56 | 57 | 58 | 59 | 60 | 61 | 62 | 63
 
 export type SquareName = 
-   'A1' | 'B1' | 'C1' | 'D1' | 'E1' | 'F1' | 'G1' | 'H1' |
-   'A2' | 'B2' | 'C2' | 'D2' | 'E2' | 'F2' | 'G2' | 'H2' |
-   'A3' | 'B3' | 'C3' | 'D3' | 'E3' | 'F3' | 'G3' | 'H3' |
-   'A4' | 'B4' | 'C4' | 'D4' | 'E4' | 'F4' | 'G4' | 'H4' |
-   'A5' | 'B5' | 'C5' | 'D5' | 'E5' | 'F5' | 'G5' | 'H5' |
-   'A6' | 'B6' | 'C6' | 'D6' | 'E6' | 'F6' | 'G6' | 'H6' |
-   'A7' | 'B7' | 'C7' | 'D7' | 'E7' | 'F7' | 'G7' | 'H7' |
-   'A8' | 'B8' | 'C8' | 'D8' | 'E8' | 'F8' | 'G8' | 'H8'
+   "A1" | "B1" | "C1" | "D1" | "E1" | "F1" | "G1" | "H1" |
+   "A2" | "B2" | "C2" | "D2" | "E2" | "F2" | "G2" | "H2" |
+   "A3" | "B3" | "C3" | "D3" | "E3" | "F3" | "G3" | "H3" |
+   "A4" | "B4" | "C4" | "D4" | "E4" | "F4" | "G4" | "H4" |
+   "A5" | "B5" | "C5" | "D5" | "E5" | "F5" | "G5" | "H5" |
+   "A6" | "B6" | "C6" | "D6" | "E6" | "F6" | "G6" | "H6" |
+   "A7" | "B7" | "C7" | "D7" | "E7" | "F7" | "G7" | "H7" |
+   "A8" | "B8" | "C8" | "D8" | "E8" | "F8" | "G8" | "H8"
 
 
 // For easy copy-pasting
