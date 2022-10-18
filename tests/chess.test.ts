3c562bbe4b4ab7cf6e10654cb2014975e7605272
Steven Nguyen <nguyeste008@students.garlandisd.net>
2022-10-18 05:51:30 -0500

    fix import

--- tests/chess.test.ts
+++ tests/chess.test.ts
@@ -18,7 +18,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-import { ValueError } from "../src/parts/python/builtin"
+import { ValueError } from "../src/python/builtin"
 import * as chess from "../src/chess"
 
 describe("Square", () => {
